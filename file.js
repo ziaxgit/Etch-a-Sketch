ce64b1524df7f1cdc340210ef16297f4f00e67ec
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2023-11-16 23:59:36 +0000

    implement dynamic gridboxes

--- file.js
+++ file.js
@@ -1,30 +1,38 @@
 let sketchPad = document.querySelector(".sketchpad");
 
-let gridSize = 20;
+let input = document.querySelector("input");
 
 // this is used to populate the grid layout
-for (let i = 0; i < gridSize; i++) {
-  let row = document.createElement("div");
-  row.classList.add("row");
+function populateGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
 
-  for (let j = 0; j < gridSize; j++) {
-    let gridBox = document.createElement("div");
-    gridBox.classList.add("gridBox");
-    gridBox.style.width = `${500 / gridSize}px`;
-    gridBox.style.height = `${400 / gridSize}px`;
-    row.appendChild(gridBox);
-    gridBox.addEventListener("mouseover", (e) => {
-      const randomBetween = (min, max) =>
-        min + Math.floor(Math.random() * (max - min + 1));
-      const r = randomBetween(0, 255);
-      const g = randomBetween(0, 255);
-      const b = randomBetween(0, 255);
-      const rgb = `rgb(${r},${g},${b})`;
-      if ((e.type = "mousedown")) {
-        gridBox.style.backgroundColor = rgb;
-      }
-    });
+    for (let j = 0; j < gridSize; j++) {
+      let gridBox = document.createElement("div");
+      gridBox.classList.add("gridBox");
+      gridBox.style.width = `${500 / gridSize}px`;
+      gridBox.style.height = `${400 / gridSize}px`;
+      row.appendChild(gridBox);
+      gridBox.addEventListener("mouseover", (e) => {
+        const randomBetween = (min, max) =>
+          min + Math.floor(Math.random() * (max - min + 1));
+        const r = randomBetween(0, 255);
+        const g = randomBetween(0, 255);
+        const b = randomBetween(0, 255);
+        const rgb = `rgb(${r},${g},${b})`;
+        if ((e.type = "mousedown")) {
+          gridBox.style.backgroundColor = rgb;
+        }
+      });
 
-    sketchPad.appendChild(row);
+      sketchPad.appendChild(row);
+    }
   }
 }
+
+input.addEventListener("input", (event) => {
+  sketchPad.innerHTML = "";
+  gridSize = event.target.value;
+  populateGrid(gridSize);
+});
